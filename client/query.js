0495a758929cc00a0b8a36ce9da5f2db314a9ef9
Berkay <>
2018-09-26 00:18:45 +0300

    first interation via NodeJS cli sdk

--- client/query.js
+++ client/query.js
@@ -53,7 +53,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 	// queryAllCars chaincode function - requires no arguments , ex: args: [''],
 	const request = {
 		//targets : --- letting this default to the peers assigned to the channel
-		chaincodeId: 'mycc',
+		chaincodeId: 'keyagreement',
 		fcn: 'getAll',
 		args: ['']
 	};
