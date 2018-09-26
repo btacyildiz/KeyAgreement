e7905a74ab43e7de06100e3cb5e7bc125a1059e5
Berkay <>
2018-09-26 22:55:28 +0300

    invoke and query modules are encapsulated in a function

--- client/query.js
+++ client/query.js
@@ -8,70 +8,87 @@
  * Chaincode query
  */
 
-var Fabric_Client = require('fabric-client');
-var path = require('path');
-var util = require('util');
-var os = require('os');
+var chaincodeQuery = function (user, queryRequest) {
 
-//
-var fabric_client = new Fabric_Client();
+	console.log(user + " user querying " + JSON.stringify(queryRequest))
 
-// setup the fabric network
-var channel = fabric_client.newChannel('mychannel');
-var peer = fabric_client.newPeer('grpc://localhost:7051');
-channel.addPeer(peer);
+	var Fabric_Client = require('fabric-client');
+	var path = require('path');
+	var util = require('util');
+	var os = require('os');
 
-//
-var member_user = null;
-var store_path = path.join(__dirname, 'hfc-key-store');
-console.log('Store path:'+store_path);
-var tx_id = null;
+	//
+	var fabric_client = new Fabric_Client();
 
-// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
-Fabric_Client.newDefaultKeyValueStore({ path: store_path
-}).then((state_store) => {
-	// assign the store to the fabric client
-	fabric_client.setStateStore(state_store);
-	var crypto_suite = Fabric_Client.newCryptoSuite();
-	// use the same location for the state store (where the users' certificate are kept)
-	// and the crypto store (where the users' keys are kept)
-	var crypto_store = Fabric_Client.newCryptoKeyStore({path: store_path});
-	crypto_suite.setCryptoKeyStore(crypto_store);
-	fabric_client.setCryptoSuite(crypto_suite);
+	// setup the fabric network
+	var channel = fabric_client.newChannel('mychannel');
+	var peer = fabric_client.newPeer('grpc://localhost:7051');
+	channel.addPeer(peer);
 
-	// get the enrolled user from persistence, this user will sign all requests
-	return fabric_client.getUserContext('user1', true);
-}).then((user_from_store) => {
-	if (user_from_store && user_from_store.isEnrolled()) {
-		console.log('Successfully loaded user1 from persistence');
-		member_user = user_from_store;
-	} else {
-		throw new Error('Failed to get user1.... run registerUser.js');
-	}
+	//
+	var member_user = null;
+	var store_path = path.join(__dirname, 'hfc-key-store');
+	console.log('Store path:' + store_path);
+	var tx_id = null;
 
-	// queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
-	// queryAllCars chaincode function - requires no arguments , ex: args: [''],
-	const request = {
+	// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+	Fabric_Client.newDefaultKeyValueStore({
+		path: store_path
+	}).then((state_store) => {
+		// assign the store to the fabric client
+		fabric_client.setStateStore(state_store);
+		var crypto_suite = Fabric_Client.newCryptoSuite();
+		// use the same location for the state store (where the users' certificate are kept)
+		// and the crypto store (where the users' keys are kept)
+		var crypto_store = Fabric_Client.newCryptoKeyStore({ path: store_path });
+		crypto_suite.setCryptoKeyStore(crypto_store);
+		fabric_client.setCryptoSuite(crypto_suite);
+
+		// get the enrolled user from persistence, this user will sign all requests
+		return fabric_client.getUserContext(user, true);
+	}).then((user_from_store) => {
+		if (user_from_store && user_from_store.isEnrolled()) {
+			console.log('Successfully loaded user1 from persistence');
+			member_user = user_from_store;
+		} else {
+			throw new Error('Failed to get user1.... run registerUser.js');
+		}
+
+		// queryCar chaincode function - requires 1 argument, ex: args: ['CAR4'],
+		// queryAllCars chaincode function - requires no arguments , ex: args: [''],
+	
+
+		// send the query proposal to the peer
+		return channel.queryByChaincode(queryRequest);
+	}).then((query_responses) => {
+		console.log("Query has completed, checking results");
+		// query_responses could have more than one  results if there multiple peers were used as targets
+		if (query_responses && query_responses.length == 1) {
+			if (query_responses[0] instanceof Error) {
+				console.error("error from query = ", query_responses[0]);
+			} else {
+				console.log("Response is ", query_responses[0].toString());
+			}
+		} else {
+			console.log("No payloads were returned from query");
+		}
+	}).catch((err) => {
+		console.error('Failed to query successfully :: ' + err);
+	});
+}
+
+// test
+if(process.argv.length > 2 && process.argv[2] == "test"){
+	chaincodeQuery("user1", 
+	{
 		//targets : --- letting this default to the peers assigned to the channel
 		chaincodeId: 'keyagreement',
 		fcn: 'getAll',
 		args: ['']
-	};
+	})
+}else{
+	console.log("Args: " + process.argv)
+	console.log("Test mode can be runned via test argument")
+}
 
-	// send the query proposal to the peer
-	return channel.queryByChaincode(request);
-}).then((query_responses) => {
-	console.log("Query has completed, checking results");
-	// query_responses could have more than one  results if there multiple peers were used as targets
-	if (query_responses && query_responses.length == 1) {
-		if (query_responses[0] instanceof Error) {
-			console.error("error from query = ", query_responses[0]);
-		} else {
-			console.log("Response is ", query_responses[0].toString());
-		}
-	} else {
-		console.log("No payloads were returned from query");
-	}
-}).catch((err) => {
-	console.error('Failed to query successfully :: ' + err);
-});
+module.exports = chaincodeQuery
