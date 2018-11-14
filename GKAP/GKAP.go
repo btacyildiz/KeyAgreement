eea5f6f8c274e1236368e072234b80a989d04f9e
berkay <berkaytacyildiz@gmail.com>
2018-11-14 18:01:54 +0300

    generating test params and validating the protocol

--- GKAP/GKAP.go
+++ GKAP/GKAP.go
@@ -11,18 +11,28 @@ import (
 // participants perform key agreement protocol
 // initialization parameters are defined before
 
-const p = 11
+/**
+p and q are large prime numbers
+g = (Zp)
+x = (Zq)
+y = g^x mod p
+k,v = (Zq) (are randomly selected)
+**/
 const q = 5
+const p = 2*q + 1
 const g = 3
 
 const k1 = 2
 const v1 = 3
 const x1 = 4
-const y1 = 6
 
-const k2 = 5
-const v2 = 6
-const x2 = 7
+var y1 = math.Mod(math.Pow(g, x1), p)
+
+const k2 = 1
+const v2 = 2
+const x2 = 3
+
+var y2 = math.Mod(math.Pow(g, x2), p)
 
 func getHashWithTimeStamp(input string) uint32 {
 	concat := strconv.FormatInt(time.Now().Unix(), 10) + input
@@ -40,7 +50,7 @@ func calcTempPubParams(k float64, v float64, x float64) (float64, float64, float
 
 	var A = math.Mod(math.Pow(g, v), p)
 	fmt.Println("W: ", w, " A: ", A, " X: ", x, " V: ", v, " Q: ", q, " ::: ", math.Mod(-4, 5))
-	var up = float64(getHashWithTimeStamp(FloatToString(w))) - A*x
+	var up = math.Abs(w - A*x) // float64(getHashWithTimeStamp(FloatToString(w))) - A*x
 	var partialOfB = (int(up) / int(v))
 	fmt.Println("Partial of B: ", partialOfB)
 	var B = math.Mod(float64(partialOfB), q)
@@ -60,7 +70,8 @@ func verifyPubVariables(w, A, B, y float64) bool {
 
 	fmt.Println("Second check is succeeded")
 
-	firstPart := math.Pow(g, float64(getHashWithTimeStamp(FloatToString(w))))
+	//firstPart := math.Pow(g, float64(getHashWithTimeStamp(FloatToString(w))))
+	firstPart := math.Mod(math.Pow(g, w), p)
 	secondPart := math.Mod((math.Pow(y, A) * math.Pow(A, B)), p)
 	fmt.Println("First: ", firstPart, " Second: ", secondPart)
 	if firstPart != secondPart {
@@ -71,10 +82,17 @@ func verifyPubVariables(w, A, B, y float64) bool {
 }
 
 func main() {
-	// STEP 1 CALCULATE
-	w, A, B := calcTempPubParams(k1, v1, x1)
+	/*
+		// STEP 1 CALCULATE
+		w, A, B := calcTempPubParams(k1, v1, x1)
 
-	verifyRes := verifyPubVariables(w, A, B, y1)
+		verifyRes := verifyPubVariables(w, A, B, y1)
+	*/
+	// STEP 2
 
-	fmt.Println("Res: ", w, A, B, " Verify Res: ", verifyRes)
+	w2, A2, B2 := calcTempPubParams(k2, v2, x2)
+
+	verifyRes2 := verifyPubVariables(w2, A2, B2, y2)
+
+	fmt.Println("Res: ", w2, A2, B2, " Verify Res2: ", verifyRes2)
 }
