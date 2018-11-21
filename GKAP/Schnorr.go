2c784cf670995cda34bd49f4a3542a697188ba37
berkay <berkaytacyildiz@gmail.com>
2018-11-21 17:48:21 +0300

    implementing Schnorr signature

---
+++ GKAP/Schnorr.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dedis/kyber"
+	"github.com/dedis/kyber/group/edwards25519"
+	"github.com/dedis/kyber/sign/schnorr"
+)
+
+func main() {
+
+	suite := edwards25519.NewBlakeSHA256Ed25519()
+	rand := suite.XOF([]byte("example"))
+	fmt.Println("Rand: ", rand.Read)
+
+	dataToBeSigned := "test1234"
+	out, err := schnorr.Sign(kyber.Random, rand, []byte(dataToBeSigned))
+}
