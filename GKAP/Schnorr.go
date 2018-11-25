eeb4b30a44baea21ff63fbbe42cbf5dee898f0a7
Berkay <>
2018-11-25 21:15:49 +0300

    keytookit

--- GKAP/Schnorr.go
+++ GKAP/Schnorr.go
@@ -12,7 +12,7 @@ func main() {
 
 	suite := edwards25519.NewBlakeSHA256Ed25519()
 	rand := suite.XOF([]byte("example"))
-	fmt.Println("Rand: ", rand.Read)
+	fmt.Println("Rand: ", rand)
 
 	dataToBeSigned := "test1234"
 	out, err := schnorr.Sign(kyber.Random, rand, []byte(dataToBeSigned))
